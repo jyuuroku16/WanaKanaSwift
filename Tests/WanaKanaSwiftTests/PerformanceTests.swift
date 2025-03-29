6dae61df5a444af47b7d13c9b98a84978a1a99dd
wuzhiying <wuzhiying11@gmail.com>
2025-03-29 14:44:30 +0800

    feat: add performance test case

--- Tests/WanaKanaSwiftTests/PerformanceTests.swift
+++ Tests/WanaKanaSwiftTests/PerformanceTests.swift
@@ -39,4 +39,44 @@ final class PerformanceTests {
         let duration = endTime.timeIntervalSince(startTime)
         #expect(duration < 1.0)
     }
+
+    @Test("Converts hiragana to katakana in less than 1 second") func testHiraganaToKatakana() async throws {
+        let startTime = Date()
+        let testString = String(repeating: "あいうえおさしすせそなにぬねのかきくけこ", count: 2500)
+        let _ = WanaKana.toKatakana(testString)
+        let endTime = Date()
+
+        let duration = endTime.timeIntervalSince(startTime)
+        #expect(duration < 1.0)
+    }
+
+    @Test("Converts katakana to hiragana in less than 1 second") func testKatakanaToHiragana() async throws {
+        let startTime = Date()
+        let testString = String(repeating: "アイウエオサシスセソナニヌネノカキクケコ", count: 2500)
+        let _ = WanaKana.toHiragana(testString)
+        let endTime = Date()
+
+        let duration = endTime.timeIntervalSince(startTime)
+        #expect(duration < 1.0)
+    }
+
+    @Test("Converts hiragana to romaji in less than 1 second") func testHiraganaToRomaji() async throws {
+        let startTime = Date()
+        let testString = String(repeating: "あいうえおさしすせそなにぬねのかきくけこ", count: 2500)
+        let _ = WanaKana.toRomaji(testString)
+        let endTime = Date()
+
+        let duration = endTime.timeIntervalSince(startTime)
+        #expect(duration < 1.0)
+    }
+
+    @Test("Converts katakana to romaji in less than 1 second") func testKatakanaToRomaji() async throws {
+        let startTime = Date()
+        let testString = String(repeating: "アイウエオサシスセソナニヌネノカキクケコ", count: 2500)
+        let _ = WanaKana.toRomaji(testString)
+        let endTime = Date()
+
+        let duration = endTime.timeIntervalSince(startTime)
+        #expect(duration < 1.0)
+    }
 }
